67ff1148f88f81aa5cc61232e51b455a05d25b7b
Ibiere <103963379+Fyngirl@users.noreply.github.com>
2023-03-30 04:32:37 -0700

    _printf

--- functions2.c
+++ functions2.c
@@ -82,9 +82,11 @@ int print_non_printable(va_list types, char buffer[],
 		if (is_printable(str[i]))
 			buffer[i + offset] = str[i];
 		else
+		{
 			offset += append_hexa_code(str[i], buffer, i + offset);
 
-			i++
+			i++;
+		}
 	}
 
 	buffer[i + offset] = '\0';
--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define UNUSED ((x) (void) (x))
+#define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
 /* Flag Modifier Macros */
